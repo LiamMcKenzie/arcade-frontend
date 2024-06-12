d20b0886352cd0b50a4b95ef5fe67cbc00409132
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-06-12 19:32:29 +1200

    REFACTOR: switch from cookies to local storage

--- js/gamesList.js
+++ js/gamesList.js
@@ -1,31 +1,72 @@
 //https://www.w3schools.com/js/js_cookies.asp
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + JSON.stringify(value) + ";" + expires + ";path=/";
+//https://medium.com/@lancelyao/browser-storage-local-storage-session-storage-cookie-indexeddb-and-websql-be6721ebe32a
+// function setCookie(name, value, days) {
+//     const date = new Date();
+//     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+//     const expires = "expires=" + date.toUTCString();
+//     document.cookie = name + "=" + JSON.stringify(value) + ";" + expires + ";path=/";
+// }
+
+// function getCookie(name)
+// {
+//     const cookieName = name + "=";
+//     const cookieArray = document.cookie.split(';');
+//     for (let i = 0; i < cookieArray.length; i++) {
+//         let cookie = cookieArray[i];
+//         while (cookie.charAt(0) == ' ') {
+//             cookie = cookie.substring(1);
+//         }
+
+
+//         if (cookie.indexOf(cookieName) == 0) {
+//             return JSON.parse(cookie.substring(cookieName.length, cookie.length));
+//         }
+//     }
+//     return "";
+
+// }
+
+// var games = getCookie('gamesList')|| [
+//     {
+//         bannerSrc: 'img/banners/frontend progress.PNG',
+//         gameName: 'Game One',
+//         developers: 'John Doe, Jane Doe',
+//         className: 'Studio 5',
+//         year: '2020',
+//         downloadLink: 'downloadables/ScaredKrow 1.1.zip',
+//         size: '50MB'
+//     },
+//     {
+//         bannerSrc: 'img/banners/Capture7.PNG',
+//         gameName: 'Game Two',
+//         developers: 'Alice Smith, Bob Johnson',
+//         className: 'Intro App Dev',
+//         year: '2018',
+//         downloadLink: 'downloadables/ScaredKrow 1.1.zip',
+//         size: '75MB'
+//     },
+//     {
+//         bannerSrc: 'img/banners/colour palette.PNG',
+//         gameName: 'Game Three',
+//         developers: 'Michael Brown, Rachel Green',
+//         className: 'Game Development',
+//         year: '2019',
+//         downloadLink: 'downloadables/ScaredKrow 1.1.zip',
+//         size: '120MB'
+//     },
+// ]
+
+
+function setLocalStorage(name, value) {
+    localStorage.setItem(name, JSON.stringify(value));
 }
 
-function getCookie(name)
-{
-    const cookieName = name + "=";
-    const cookieArray = document.cookie.split(';');
-    for (let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1);
-        }
-
-
-        if (cookie.indexOf(cookieName) == 0) {
-            return JSON.parse(cookie.substring(cookieName.length, cookie.length));
-        }
-    }
-    return "";
+function getLocalStorage(name) {
+    return JSON.parse(localStorage.getItem(name));
 
 }
 
-var games = getCookie('gamesList')|| [
+var games = getLocalStorage('gamesList') || [
     {
         bannerSrc: 'img/banners/frontend progress.PNG',
         gameName: 'Game One',
@@ -54,6 +95,8 @@ var games = getCookie('gamesList')|| [
         size: '120MB'
     },
 ]
+
+
 
 function createTableRow(game) {
     const tr = document.createElement('tr');
@@ -109,7 +152,7 @@ window.onload = addAllGameRows; //loads all the games when the page is loaded
 function addGameRow(bannerSrc, gameName, developers, className, year, downloadLink) {
     const tableBody = document.getElementById('game-table-body');
     const newGame = {
-        bannerSrc: bannerSrc,
+        bannerSrc: 'img/banners/frontend progress.PNG',
         gameName: gameName,
         developers: developers,
         className: className,
@@ -118,7 +161,7 @@ function addGameRow(bannerSrc, gameName, developers, className, year, downloadLi
         size: '60MB'
     };
     games.push(newGame);
-    setCookie('gamesList', games, 7);
+    setLocalStorage('gamesList', games, 7);
     const newRow = createTableRow(newGame);
     tableBody.appendChild(newRow);
 }
