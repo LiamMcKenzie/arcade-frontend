0547eb31daaf137e2880d0b404a17d682b8616cb
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-06-17 18:25:17 +1200

    ADD: darkmode icon changes color

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,10 +2,24 @@ const toggleDarkMode = () => {
     document.body.classList.toggle('dark-mode');
     const isDarkMode = document.body.classList.contains('dark-mode');
     localStorage.setItem('darkMode', isDarkMode);
+
+    updateButtonIcon(isDarkMode)
 };
 
 document.addEventListener("DOMContentLoaded", (event) => {
     if (localStorage.getItem('darkMode') === 'true') {
         document.body.classList.add('dark-mode');
     }
-  });
+
+    updateButtonIcon(localStorage.getItem('darkMode') === 'true');
+});
+
+function updateButtonIcon(isDarkMode) {
+    const darkModeIcon = document.querySelector('.toggle-dark-mode img');
+
+    if (isDarkMode) {
+        darkModeIcon.src = 'img/circle-half-stroke-solid(dark).svg';
+    }else {
+        darkModeIcon.src = 'img/circle-half-stroke-solid(light).svg';
+    }
+};
